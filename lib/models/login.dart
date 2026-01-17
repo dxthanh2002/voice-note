a26fd74104cd1af7e783c924c1bf9082367c8a72
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2026-01-17 17:29:35 +0700

    delete dead code

--- lib/models/login.dart
+++ lib/models/login.dart
@@ -1,4 +1,13 @@
-import 'user.dart';
+class UserInfo {
+  final String id;
+  final String type;
+
+  UserInfo({required this.id, required this.type});
+
+  factory UserInfo.fromJson(Map<String, dynamic> json) {
+    return UserInfo(id: json['id'] as String, type: json['type'] as String);
+  }
+}
 
 class LoginResponse {
   final String accessToken;
